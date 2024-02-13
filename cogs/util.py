ea6190b66e35423c30c4f7abd4d3b8eb96a536ec
unknown <lzzy2747@naver.com>
2024-02-13 23:09:23 +0900

    style

--- cogs/util.py
+++ cogs/util.py
@@ -1,13 +1,16 @@
+from datetime import datetime
+
+import aiohttp
 import discord
+import requests
+from bs4 import BeautifulSoup
 from discord.ext import commands
+from lxml import html
 from simpcalc.errors import BadArgument, Overflow
 from simpcalc.simpcalc import Calculate
-import aiohttp
-from bs4 import BeautifulSoup
-from lxml import html
-import requests
+
 from config import serviceKey
-from datetime import datetime
+
 
 class Util(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -37,29 +40,44 @@ class Util(commands.Cog):
     @commands.slash_command(name="한강", description="한강 수온을 알려줍니다.")
     async def river(self, ctx: discord.ApplicationContext):
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://api.hangang.life/') as response:
+            async with session.get("https://api.hangang.life/") as response:
                 if response.status == 200:
                     data = await response.json()
-                    temp = data['DATAs']['DATA']["HANGANG"]["노량진"]["TEMP"]
+                    temp = data["DATAs"]["DATA"]["HANGANG"]["노량진"]["TEMP"]
 
-                    await ctx.respond(f'{temp}°C')
+                    await ctx.respond(f"{temp}°C")
 
-    @commands.slash_command(name='출몰시간', description="출몰 시간을 알려줍니다.")
+    @commands.slash_command(name="출몰시간", description="출몰 시간을 알려줍니다.")
     async def sun_time(self, ctx: discord.ApplicationContext):
-        url = 'http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo'
-        params ={'serviceKey' : serviceKey, 'locdate': datetime.now().strftime('%Y%m%d'), 'location': '서울'}
+        url = "http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo"
+        params = {
+            "serviceKey": serviceKey,
+            "locdate": datetime.now().strftime("%Y%m%d"),
+            "location": "서울",
+        }
         response = requests.get(url, params=params)
 
         content = response.text
 
-        xml_object = BeautifulSoup(content, 'lxml-xml')
-        sunrise = xml_object.find('body').find('items').find('item').find('sunrise').text
-        sunset = xml_object.find('body').find('items').find('item').find('sunset').text
+        xml_object = BeautifulSoup(content, "lxml-xml")
+        sunrise = (
+            xml_object.find("body").find("items").find("item").find("sunrise").text
+        )
+        sunset = xml_object.find("body").find("items").find("item").find("sunset").text
 
         embed = discord.Embed()
-        embed.add_field(name='☀️ 일출', value=f'{int(sunrise[:2])}시 {int(sunrise[2:])}분', inline=False)
-        embed.add_field(name="🌙 일몰", value=f'{int(sunset[:2])}시 {int(sunset[2:])}분', inline=False)
+        embed.add_field(
+            name="☀️ 일출",
+            value=f"{int(sunrise[:2])}시 {int(sunrise[2:])}분",
+            inline=False,
+        )
+        embed.add_field(
+            name="🌙 일몰",
+            value=f"{int(sunset[:2])}시 {int(sunset[2:])}분",
+            inline=False,
+        )
         await ctx.respond(embed=embed)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Util(bot=bot))
