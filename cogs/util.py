4bed092a55852fe7cc87dd493c60d490dbe305dd
unknown <lzzy2747@naver.com>
2024-02-13 22:21:46 +0900

    util

--- cogs/util.py
+++ cogs/util.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from simpcalc.errors import BadArgument, Overflow
 from simpcalc.simpcalc import Calculate
+import aiohttp
 
 
 class Util(commands.Cog):
@@ -23,16 +24,21 @@ class Util(commands.Cog):
             result = await Calculate().calculate(expr=expression)
             await ctx.respond(result)
         except Overflow:
-            await ctx.respond(
-                "결과 값이 너무 큽니다.", ephemeral=True, delete_after=5.0
-            )
+            await ctx.respond("결과 값이 너무 큽니다.", ephemeral=True)
         except BadArgument:
             await ctx.respond(
-                "잘못된 수식 입력입니다. 다시 입력해주세요.",
-                ephemeral=True,
-                delete_after=5.0,
+                "잘못된 수식 입력입니다. 다시 입력해주세요.", ephemeral=True
             )
 
+    @commands.slash_command(name="한강", description="한강 수온을 알려줍니다.")
+    async def river(self, ctx: discord.ApplicationContext):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.hangang.life/') as response:
+                if response.status == 200:
+                    data = await response.json()
+                    temp = data['DATA']["HANGANG"]["노량진"]["TEMP"]
+
+                    await ctx.respond(f'{temp}°C')
 
 def setup(bot: commands.Bot):
     bot.add_cog(Util(bot=bot))
