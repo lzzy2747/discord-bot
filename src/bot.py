652e83831c955a20dcb8513f9e8d7e90584ade0a
unknown <lzzy2747@naver.com>
2024-02-14 06:26:31 +0900

    clean up

--- src/bot.py
+++ src/bot.py
@@ -5,24 +5,25 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 
-class Main(commands.AutoShardedBot):
+class Bot(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(help_commands=None, intents=Intents.all())
-        load_dotenv(dotenv_path="./.env")
+
+        load_dotenv(dotenv_path="../.env")
 
     async def on_ready(self):
         print(f"Logged as {self.user.name}")
-        print(f"Loaded {len(self.extensions)} extensions")
+        print(f"Loaded {len(bot.extensions)} extensions")
 
     def run(self):
-        super().run(getenv(key="BOT_TOKEN"))
+        return super().run(getenv(key="BOT_TOKEN"))
 
 
-bot = Main()
+bot = Bot()
 
 if __name__ == "__main__":
-    for filename in listdir("./cogs"):
+    for filename in listdir("./commands"):
         if filename.endswith(".py") and not filename.startswith("__"):
-            bot.load_extension(f"cogs.{filename[:-3]}")
+            bot.load_extension(f"commands.{filename[:-3]}")
 
     bot.run()
