ff63c14fe1d5d65a0b7e08fd2bcb1c7c56f42d70
unknown <lzzy2747@naver.com>
2024-02-14 07:29:51 +0900

    fix

--- src/function/disaster.py
+++ src/function/disaster.py
@@ -7,10 +7,13 @@ from function.https import get_request
 load_dotenv(dotenv_path="../.env")
 
 URL: str = "http://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List"
+PAGE_NUMBER: str = "1"
+NUMBER_OF_ROWS: str = "1"
+
 params: dict = {
     "serviceKey": getenv("SERVICE_URL"),
-    "pageNo": "1",
-    "numOfRows": "1",
+    "pageNo": PAGE_NUMBER,
+    "numOfRows": NUMBER_OF_ROWS,
     "type": "json",
 }
 
