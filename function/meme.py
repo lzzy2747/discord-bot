2e2d42972b4e09720c8397fd54c20068f5363c5e
unknown <lzzy2747@naver.com>
2024-02-15 08:53:02 +0900

    fix bugs

--- function/meme.py
+++ function/meme.py
@@ -2,5 +2,5 @@ from utils.https import async_get
 
 
 async def meme(category: str):
-    data = await async_get(url=f"https://api.waifu.pics/sfw/{category}")
+    data = await async_get(url=f"https://api.waifu.pics/sfw/{category}", headers=None)
     return data["url"]
