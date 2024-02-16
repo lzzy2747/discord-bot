50b0e1e93d2f574abf2e0df7300e72997bb0902f
unknown <lzzy2747@naver.com>
2024-02-16 23:34:27 +0900

    update

--- cogs/mod.py
+++ cogs/mod.py
@@ -10,6 +10,134 @@ class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    async def cog_app_command_error(
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
+    ) -> None:
+        if isinstance(error, commands.MissingPermissions) or isinstance(
+            error, commands.BotMissingPermissions
+        ):
+            await interaction.response.defer(thinking=True, ephemeral=True)
+            await interaction.followup.send(
+                embed=await error_embed(description="권한이 부족합니다."),
+                ephemeral=True,
+            )
+
+        raise error
+
+    @app_commands.command(name="청소", description="메시지를 청소합니다.")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.rename(amount="개수")
+    @app_commands.describe(amount="청소할 개수")
+    async def clear(self, interaction: discord.Interaction, amount: int):
+        if not 1 < amount < 100:
+            await interaction.response.defer(thinking=True, ephemeral=True)
+            return await interaction.followup.send(
+                embed=await error_embed(
+                    description="1개 이상 100개 이하에 메시지만 지울 수 있습니다."
+                ),
+                ephemeral=True,
+            )
+
+        await interaction.response.defer(thinking=True, ephemeral=False)
+        await interaction.channel.purge(limit=amount)
+        await interaction.channel.send(
+            embed=await success_embed(description=f"{amount}건에 메시지를 지웠습니다."),
+            delete_after=5.0,
+        )
+
+    @app_commands.command(name="슬로우모드", description="슬로우모드를 걸어줍니다.")
+    @app_commands.rename(channel="채널", duration="기간")
+    @app_commands.describe(channel="채널", duration="기간 (초)")
+    @app_commands.checks.has_permissions(manage_channels=True)
+    async def slowmode(
+        self,
+        interaction: discord.Interaction,
+        channel: discord.TextChannel = None,
+        *,
+        duration: int,
+    ):
+        if channel is None:
+            channel = interaction.channel
+
+        if duration == 0:
+            await interaction.response.defer(thinking=True, ephemeral=False)
+            await interaction.channel.edit(slowmode_delay=None)
+            return await interaction.followup.send(
+                embed=await success_embed(
+                    description=f"{channel.mention}에 슬로우모드를 해제했습니다."
+                ),
+                ephemeral=False,
+            )
+
+        if duration < 21600:
+            await interaction.response.defer(thinking=True, ephemeral=True)
+            return await interaction.followup.send(
+                embed=await error_embed(description="21600초 미만으로 입력하세요."),
+                ephemeral=True,
+            )
+
+        await interaction.response.defer(thinking=True, ephemeral=False)
+        await interaction.channel.edit(slowmode_delay=duration)
+        await interaction.followup.send(
+            embed=await success_embed(
+                description=f"{channel.mention}에 {duration}초에 슬로우모드를 적용했습니다."
+            ),
+            ephemeral=False,
+        )
+
+    @app_commands.command(name="킥", description="맴버를 킥합니다.")
+    @app_commands.rename(member="맴버", reason="사유")
+    @app_commands.describe(member="킥할 맴버", reason="킥하는 사유")
+    @app_commands.checks.has_permissions(kick_members=True)
+    async def kick(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        reason: str = None,
+    ):
+        if member.id == interaction.user.id:
+            await interaction.response.defer(thinking=True, ephemeral=True)
+            return await interaction.followup.send(
+                embed=await error_embed(description="본인을 킥할 수 없습니다."),
+                ephemeral=True,
+            )
+
+        await interaction.response.defer(thinking=True, ephemeral=False)
+        await member.kick(reason=reason)
+        await interaction.followup.send(
+            embed=await success_embed(
+                description=f"{member.mention}을 서버에서 킥했습니다."
+            ),
+            ephemeral=False,
+        )
+
+    @app_commands.command(name="밴", description="맴버를 밴합니다.")
+    @app_commands.rename(member="맴버", reason="사유")
+    @app_commands.describe(member="밴할 맴버", reason="밴하는 사유")
+    @app_commands.checks.has_permissions(ban_members=True)
+    async def ban(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        reason: str = None,
+    ):
+        if member.id == interaction.user.id:
+            await interaction.response.defer(thinking=True, ephemeral=True)
+            return await interaction.followup.send(
+                embed=await error_embed(description="본인을 밴할 수 없습니다."),
+                ephemeral=True,
+            )
+
+        await interaction.response.defer(thinking=True, ephemeral=False)
+        await member.ban(delete_message_days=7, reason=reason)
+        await interaction.followup.send(
+            embed=await success_embed(
+                description=f"{member.mention}을 서버에서 밴했습니다."
+            ),
+            ephemeral=False,
+        )
+
+    # TODO add timeout command
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Mod(bot=bot))
