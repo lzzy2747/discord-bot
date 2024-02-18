2c10d56968a86430af5cab4a61c5238614657cba
lzzy2747 <lzzy2747@naver.com>
2024-02-19 01:13:57 +0900

    ssl bug temp fix

--- tool/api.py
+++ tool/api.py
@@ -4,7 +4,7 @@ from aiohttp import ClientSession
 
 
 async def get_req(u: str, h: Optional[dict] = None):
-    async with ClientSession(headers=h) as session:
+    async with ClientSession(headers=h, trust_env=True) as session:
         async with session.get(url=u) as response:
             if response.status == 200:
                 d = await response.json(content_type=None)
@@ -14,7 +14,7 @@ async def get_req(u: str, h: Optional[dict] = None):
 
 
 async def post_req(u: str, h: Optional[dict] = None, d: Optional[dict] = None):
-    async with ClientSession(headers=h) as session:
+    async with ClientSession(headers=h, trust_env=True) as session:
         async with session.post(url=u, data=d) as response:
             if response.status == 200:
                 dr = await response.json(content_type=None)
